6654850948ca309486b594c111454fb70a10bb2d
killhello <killhello@users.noreply.github.com>
2026-08-28 19:19:25 +0800

    fix: wifi_portal缺失头文件
    - esp_mac.h: MACSTR/MAC2STR
    - dogtag_ui.h: dogtag_ui_enter_silent
    - nvs_flash.h: nvs_flash_init/erase
    - lwip/ip_addr.h: IP4_ADDR

--- main/wifi_portal.c
+++ main/wifi_portal.c
@@ -1,18 +1,22 @@
 #include "wifi_portal.h"
 #include "dogtag_state.h"
+#include "dogtag_ui.h"
 #include "dogtag_audio.h"
 
 #include "esp_log.h"
+#include "esp_mac.h"
 #include "esp_wifi.h"
 #include "esp_netif.h"
 #include "esp_event.h"
 #include "esp_http_server.h"
 #include "lwip/err.h"
 #include "lwip/sys.h"
+#include "lwip/ip_addr.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "nvs.h"
+#include "nvs_flash.h"
 #include "cJSON.h"
 #include <string.h>
 
